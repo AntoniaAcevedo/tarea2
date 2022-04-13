2c39ade20800ad3c6c2cf3b312b5005d94decb41
AlexandraJMV <alexandra.mendoza.v@mail.pucv.cl>
2022-04-13 16:19:27 -0400

    .

--- main.c
+++ main.c
@@ -5,54 +5,65 @@
 
 int main (void)
 {
-       // ¬.¬
-    long selec;
-    long selec2;
-
-
-    printf("MENU RAWR XDDDDDDDDDDDDDDD/n/n");
-
-    printf(
-        "1. Importar productos/n"
-        "2. Exportar productos/n"
-        "3. Agregar productos/n"
-        "4. Buscar producto por criterio/n"
-        "5. Mostrar todos los productos/n"
-        "6. Operaciones de carrito/n"
-        "7. Salir del programa/n"
-
-        "/nIngrese lo que desea hacer:/n"
-    );
-
-    scanf("%ld", &selec);
-    
     while (1){
+
+       // ¬.¬
+        long selec;
+        long selec2;
+        system("cls");
+        printf("MENU RAWR XDDDDDDDDDDDDDDD\n\n");
+
+        printf(
+            "1. Importar productos\n"
+            "2. Exportar productos\n"
+            "3. Agregar productos\n"
+            "4. Buscar producto por criterio\n"
+            "5. Mostrar todos los productos\n"
+            "6. Operaciones de carrito\n"
+            "7. Salir del programa\n"
+
+            "\nIngrese lo que desea hacer:\n"
+        );
+
+        scanf("%ld", &selec);
+        getchar();
+
         switch (selec)
         {
             case 1:
+                puts("Ingrese el nombre del archivo que desea importar");
+                getchar();
             break;
             case 2:
+                puts("lol");
+                getchar();
             break;
             case 3:
+                puts("lol");
+                getchar();
             break;
             case 4:
+                puts("lol");
+                getchar();
             break;
             case 5:
+                puts("lol");
+                getchar();
             break;
             case 6:
+                puts("lol");
+                getchar();
             break;
             case 7:
                 puts("Saliendo del programa...");
                 getchar();
+                return 0;
             break;
 
             default:
-                printf("Valor ingresado no valido!/n");
+                printf("Valor ingresado no valido!\n");
                 getchar();
 
         }
     }
-
-
-    return  0;
 }
